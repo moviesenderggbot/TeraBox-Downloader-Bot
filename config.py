2f75e442ea3a859b669079aed202f0937e01bb80
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-08-05 00:33:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # API credentials
-API_ID = 123456
-API_HASH = "DWSC65HGFswcf56g8ergjhgdw54586"
-BOT_TOKEN = "123456:abcdekgfiJLKMNOPqjngghdf"
+API_ID = "22620068"
+API_HASH = "11e2c113078324f7e36688baa86c3911"
+BOT_TOKEN = "7229648232:AAE7aTxsrPgOp34PPUh66N8Bg_kbqiUfEso"
 
 # Redis configuration
 REDIS_CONFIG = {
@@ -22,4 +22,4 @@ COOKIE = {
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 
 # List of bot admins
-ADMINS = [803003146]
+ADMINS = [6062527012]
